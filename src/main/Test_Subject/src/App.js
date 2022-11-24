0f68f785b29ca5d68cff4d1603f68292863c4419
strong-minsu <dlalstn5789@gmail.com>
2022-11-24 13:39:08 +0900

    전체 점검

--- src/main/Test_Subject/src/App.js
+++ src/main/Test_Subject/src/App.js
@@ -5,7 +5,6 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-// import ListBoard from "./pages/ListBoard";
 import Writing from "./pages/Writing";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      {/* <Route path="/board" element={<ListBoard />}></Route> */}
       <Route path="/writing" element={<Writing />}></Route>
     </Routes>
   );
